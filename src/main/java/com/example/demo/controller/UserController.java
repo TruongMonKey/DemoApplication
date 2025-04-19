88a2f803ffa5bd06b6fecb4450361de21f3be7d5
Monkeyy <truongcot1907@gmail.com>
2025-04-19 16:40:45 +0700

    new update

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @Controller
+// public class UserController {
+
+//     @RequestMapping("/")
+//     public String getHomePage() {
+//         return "Hello from controller";
+//     }
+// }
+
+@RestController
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return this.userService.handleHello();
+    }
+}
