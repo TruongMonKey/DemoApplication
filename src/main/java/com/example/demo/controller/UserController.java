1b6fd02c5401afeb7858c3d557f6bb28f075b502
Monkeyy <truongcot1907@gmail.com>
2025-04-22 23:36:50 +0700

    update new

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,37 +10,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.domain.User;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 
 @Controller
 public class UserController {
-    private UserService userService;
-    private User user;
+    private final UserService userService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
     }
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("monkey", test);
+        List<User> arrUsers = this.userService.getAllUsersByEmail("truong@gmail.com");
+        System.out.println(arrUsers);
+        model.addAttribute("monkey", "test");
         model.addAttribute("hoidanit", "from control with model");
         return "hello";
     }
 
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
+        List<User> users = this.userService.getAllUsers();
+        model.addAttribute("users1", users);
+        return "admin/user/table-user"; // sẽ map tới create.jsp
+    }
+
+    @RequestMapping("/admin/user/create")
+    public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/create"; // sẽ map tới create.jsp
     }
 
-    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User monkey) {
-        System.out.println("Run here" + monkey);
-        return "hello"; // sẽ map tới create.jsp
+        this.userService.handleSaveUser(monkey);
+        return "redirect:/admin/user"; // sẽ map tới create.jsp
     }
-
 }
 
 // @RestController
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,22 +1,29 @@
 package com.example.demo.service;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
 import com.example.demo.domain.User;
+import com.example.demo.repository.UserRepository;
 
 @Service
 public class UserService {
-    public String handleHello() {
-        return "Hello from service";
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
-    public User getCurrentUser() {
-        return new User(
-                1L,
-                "monkey@gmail.com",
-                "123456",
-                "123 Banana Street",
-                "0123456789");
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
     }
 
+    public List<User> getAllUsersByEmail(String email) {
+        return this.userRepository.findByEmail(email);
+    }
+
+    public User handleSaveUser(User user) {
+        User users = this.userRepository.save(user);
+        return users;
+    }
 }
